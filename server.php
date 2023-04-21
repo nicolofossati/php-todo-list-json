24cce946f9c11ee1722d00aa223dc2978e1d6525
nicolo fossati <nicofossa01@gmail.com>
2023-04-21 13:45:01 +0200

    Aggiunta file todoList.json che funziona da database

--- server.php
+++ server.php
@@ -1,30 +1,17 @@
 <?php
 
-$todoList = [
-    [
-        'task' => 'Fare la spesa',
-        'done' => true
-    ],
-    [
-        'task' => 'Studiare',
-        'done' => false
-    ],
-    [
-        'task' => 'Passare alle poste',
-        'done' => false
-    ],
-    [
-        'task' => 'Ritirare i libri',
-        'done' => true
-    ],
-    [
-        'task' => 'Prenotare al ristorante',
-        'done' => true
-    ]
-];
+if (file_exists('todoList.json')) {
+    $json_to_string = file_get_contents('todoList.json'); //crea la stringa del file todoList.json
+    $todoList = json_decode($json_to_string, true); //decodifica da file json ad array PHP
+} else {
+    $todoList = [];
+}
+
 
 if (isset($_POST['todoInput'])) {
     $todoList[] = ['task' => $_POST['todoInput'], 'done' => false];
+    $string_to_json = json_encode($todoList);
+    file_put_contents('todoList.json', $string_to_json);
 }
 
 header('content-type: application/json');
